fd5ef0f6dc0bb73b37a9d38423d95b79d890c10d
eunsookim1 <92eunsoo@gmail.com>
2023-08-30 21:37:57 -0700

    add a new route for category

--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,15 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
-  
+  # resources :about, only: [:about]
     get '/about', to: 'about#index'
   
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    # resources :categories
+    resources :categories, except: [:edit, :update, :show]
+    
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
