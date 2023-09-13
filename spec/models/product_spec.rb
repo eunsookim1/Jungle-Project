14dde7dee050af5de12a03c95f6ccb0b66c8093d
eunsookim1 <92eunsoo@gmail.com>
2023-09-13 11:07:46 -0700

    validations for product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it 'saves successfully with all fields set' do
+      @category = Category.create(name: 'Example Category') # create a new category
+      @product = Product.new(
+        name: 'Example Product',
+        price: 10.99,
+        quantity: 5,
+        category: @category
+      )
+      expect(@product.save).to be(true)
+    end
+
+    it 'validates presence of name' do
+    # validates :name, presence: true
+      @product = Product.new(
+        name: nil,
+        price: 10.99,
+        quantity: 5,
+        category: @category
+        )
+
+        @product.valid?
+        # expect (@product.errors.full_messages).to be()
+        # puts @product.errors.full_messages.to_sentence
+        expect(@product.errors.full_messages.to_sentence).to eq("Category must exist, Name can't be blank, and Category can't be blank")
+    end
+
+    it 'validates presence of price' do
+      # validates :price, presence: true
+      @product = Product.new(
+        name: 'Product',
+        price: nil,
+        quantity: 5,
+        category: @category
+        )
+
+        @product.valid?
+        expect(@product.errors.full_messages.to_sentence).to eq("Category must exist and Category can't be blank")
+    end
+
+    it 'validates presence of quantity' do
+      # validates :quantity, presence: true
+      @product = Product.new(
+        name: 'Product',
+        price: 10.99,
+        quantity: nil,
+        category: @category
+      )
+      @product.valid?
+      expect(@product.errors.full_messages.to_sentence).to eq("Category must exist, Quantity can't be blank, and Category can't be blank")
+    end
+
+    it 'validates presence of category' do
+      # validates :category, presence: true
+      @product = Product.new(
+        name: 'Product',
+        price: 10.99,
+        quantity: 5,
+        category: nil
+      )
+      @product.valid?
+      expect(@product.errors.full_messages.to_sentence).to eq("Category must exist and Category can't be blank")
+    end
+  end
 end
